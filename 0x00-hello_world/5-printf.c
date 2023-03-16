57795f0fa844694fcc39d9d4d9a17d070ce62159
NigelT97 <nigelmj97@gmail.com>
2023-03-16 13:34:29 -0700

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry in to programme
+* print using function printf
+* Return: value 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
