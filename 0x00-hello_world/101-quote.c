2546f2246f96d352dbceded8af57cd87ccad9fab
NigelT97 <nigelmj97@gmail.com>
2023-03-16 14:38:11 -0700

    quoted

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o`echo $CFILE | cut -d. -f1`.s
+#include <stdio.h>
+/**
+* main -entry of programme
+* print out string
+* Return:this time its 1 and not zero
+*/
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+puts(str);
+return (1);
+}
