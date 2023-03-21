46ed122bd4720c81f5b69a6cfb41db3c4b0e3f5a
NigelT97 <nigelmj97@gmail.com>
2023-03-21 11:57:28 -0700

    error fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,6 @@
  * print_alphabet from a to z
  * Return: zero
  */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-/**
- * print_alphabet -  for calling out a toz printing
- */
 void print_alphabet(void)
 {
 int l;
