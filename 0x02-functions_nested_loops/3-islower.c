007c818cda31ede35d3242f6ae572fafb0705aca
NigelT97 <nigelmj97@gmail.com>
2023-03-21 12:52:19 -0700

    question 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - to check if lower letter
+ * @c: lower case desription
+ * Return: 1 if `c` lower case
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	return (0);
+}
