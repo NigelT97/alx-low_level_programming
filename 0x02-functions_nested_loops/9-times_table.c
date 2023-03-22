8faa520d12aa2362f7e91ed3d3f179ee077d5627
NigelT97 <nigelmj97@gmail.com>
2023-03-21 18:10:04 -0700

    question 10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - time table creation
+ */
+void times_table(void)
+{
+	int num, product, mult;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
+		{
+			_putchar(',');
+			_putchar(' ');
+			product = num * mult;
+			if (product <= 9)
+				_putchar('');
+			else
+				_putchar((product / 10) + 48);
+			_putchar((product % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
