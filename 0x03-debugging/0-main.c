102a38911a5258af55ef75a42716b1dac63cf644
NigelT97 <NigelT97>
2023-03-23 18:41:20 +0000

    error fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
  * main - tests the function positive or negative
  * Return: zero always
  */
+
 int main(void)
 {
 	int i;
 
 	i = 98 % 2;
 	positive_or_negative(i);
+
 	return (0);
 }
