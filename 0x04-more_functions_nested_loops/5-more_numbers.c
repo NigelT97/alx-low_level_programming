8fea2d99c4387457c2417d4d6449b677e317c384
NigelT97 <NigelT97>
2023-03-23 21:20:30 +0000

    minor fix on 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,13 @@ void more_numbers(void)
 {
 	int n, a;
 
-	for (n = 0; n < 11; n++)
+	for (n = 0; n < 11; ++n)
 	{
 		a = 0;
 
 		do {
 			_putchar(a + 48);
-			a++;
+			++a;
 		} while (a < 15);
 
 		_putchar('\n');
