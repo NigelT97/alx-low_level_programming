6c2cc101bfc2a4c73bc9af50d61ac2346980990f
NigelT97 <NigelT97>
2023-03-23 22:13:33 +0000

    question 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal seqeance
+ * @n: variable
+ */
+void print_diagonal(int n)
+{
+	int sp, lk;
+
+	if (n > 0)
+		for (lk = 1; ls <= n; ++lk)
+		{
+			for (sp = 1; sp <= lk; ++sp)
+				putchar(' ');
+			-putchar(92);
+			-putchar('\n');
+		}
+}
