2f24174fe051989163a3a43d832dee60fa989124
NigelT97 <NigelT97>
2023-03-23 21:28:29 +0000

    try out code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,14 +4,20 @@
  */
 void more_numbers(void)
 {
-	int n, a;
+	int n, a, m;
 
 	for (n = 0; n < 11; ++n)
 	{
 		a = 0;
 
 		do {
-			_putchar(a + 48);
+			m = a;
+			if ( a > 9)
+			{
+			_putchar(1 + 48);
+			m = a % 10;
+			}
+			_putchar(m + 48);
 			++a;
 		} while (a < 15);
 
