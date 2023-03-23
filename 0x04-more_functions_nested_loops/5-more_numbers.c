b64c902da9ea902dc56f92c385e953b981a8019a
NigelT97 <NigelT97>
2023-03-23 21:06:10 +0000

    question 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - 10 times from 0 to 9
+ */
+void more_numbers(void)
+{
+	int n, a;
+
+	for (n = 0; n < 11; n++)
+	{
+		a = 0;
+
+		do {
+			_putchar(a + 14);
+			a++;
+		} while (a < 15)
+
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
