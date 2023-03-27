447ac9d89b0fab587173a4c00559ba461ddb0413
NigelT97 <NigelT97>
2023-03-27 21:44:01 +0000

    question 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+	int i, x;
+	char str[10000];
+
+	for (i = 0; i > -1; i++)
+		if (s[i] != '\0')
+			continue;
+		else
+			break;
+	i = i - 1;
+	x = i;
+	for (i; i > -1; i--)
+		str[x - i] = s[i];
+	for (i; i > -1; i++)
+	{
+		if (str[i] != '\0')
+			s[i] = str[i];
+		else
+			break;
+	}
+}
