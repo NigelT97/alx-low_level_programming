2dd66da147bd2f78046a56c70fedc44474a744c7
NigelT97 <NigelT97>
2023-03-27 20:10:13 +0000

    question 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - prints out to stdout
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < 100; ++i)
+	{
+		if (str[i] != '\0')
+			putchar(str[i]);
+		else
+			break;
+	}
+
+	printf("\n");
+}
+
