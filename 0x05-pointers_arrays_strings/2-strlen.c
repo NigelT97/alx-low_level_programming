40a56d0deba256768784b47407e434a9a5ee7e3d
NigelT97 <NigelT97>
2023-03-27 19:40:52 +0000

    error fix for 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,16 @@
  */
 int _strlen(char *s)
 {
-	int x;
-	char str;
+	int x, n;
 
-	str = *s;
-	x = strlen(str);
-	return (x);
+	for (x = 0; x != 900; ++x)
+	{
+		if (s[x] != '\0')
+			continue;
+		else
+			break;
+	}
+	n = x - 1;
+	return (n);
 }
 
