35312d7ab14c95be1d59f9d81f258342e6ccf9a6
NigelT97 <NigelT97>
2023-03-27 20:54:08 +0000

    4th qn error fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,19 @@
  */
 void print_rev(char *s)
 {
-	int i, x;
+	int i, x, n;
 
 	for (x = 0; x > -1; ++x)
 	{
 		if (x != '\0')
 			continue;
 		else
+		{
+			n = x;
 			break;
+		}
 	}
-	for (i = x; i > -1; --i)
+	for (i = n; i > -1; --i)
 		putchar(s[i]);
 	putchar('\n');
 }
