8ba389a1de8b012d101ee20cce678c408fffa627
NigelT97 <NigelT97>
2023-03-30 20:03:46 +0000

    error fix in 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n; ++i)
-		dest[i] = src[i];
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			break;
 
 	return (dest);
 }
