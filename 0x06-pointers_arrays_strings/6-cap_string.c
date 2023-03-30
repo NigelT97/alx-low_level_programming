b2faeadc94cec734a84d0abbadc3d379e6b0f450
NigelT97 <NigelT97>
2023-03-30 22:04:05 +0000

    error fix on 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,30 @@
 #include "main.h"
 /**
  * *cap_string - capitalize words
- * @x: string
+ * @s: string
  * Return: the capitalized string
  */
-char *cap_string(char *x)
+char *cap_string(char *s)
 {
 	int i;
 
-	for (i = 0; x[i] != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; ++i)
 	{
-		if (x[i - 1] == 32 || x[i - 1] == 9 || x[i - 1] == 10 ||
-				x[i - 1] == 44 || x[i - 1] == 59 || x[i - 1] == 46 ||
-				x[i - 1] == 33 || x[i - 1] == 63 || x[i - 1] == 34 ||
-				x[i - 1] == 40 || x[i - 1] == 41 || x[i - 1] == 123 ||
-				x[i - 1] == 124)
-			x[i] = x[i] - 32;
-		else if ((x[0] >= 97 && x[0] <= 122))
-			x[0] = x[0] - 32;
-		else
-			x[i] = x[i];
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
+			}
+		}
 	}
-	return (x);
+	return (s);
 }
