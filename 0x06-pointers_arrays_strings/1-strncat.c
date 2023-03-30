478c25e937f3b327be569cb5f7774038ad534afe
NigelT97 <NigelT97>
2023-03-30 19:27:06 +0000

    error fix to 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; ++i)
 		continue;
 
-	for (j = 0; j < n; ++j)
+	for (j = 0; j < n; ++j, ++i)
 	{
 		if (src[j] != '\0')
 			dest[i] = src[j];
