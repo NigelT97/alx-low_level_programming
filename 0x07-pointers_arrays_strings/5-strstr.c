44e5be2bbbbc51a44e7373617bffd233f91a1ba1
NigelT97 <NigelT97>
2023-04-03 18:22:43 +0000

    question 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strstr - lacates a substring
+ * @haystack: where to find substring
+ * @needle: substring to be found
+ * Return: beginner of substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; haystack++)
+	{
+		for (j = 0; needle[i] != '\0'; needle++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
