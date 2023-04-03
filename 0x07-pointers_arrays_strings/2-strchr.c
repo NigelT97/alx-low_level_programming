ebcbbcab08a4741908d6792fe1b7d7582ed62e9a
NigelT97 <NigelT97>
2023-04-03 13:23:29 +0000

    questiion 2 error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,31 +7,8 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i, n, j;
-	char y[10000];
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		y[i] = s[i];
-		continue;
-	}
-
-	for (n = 0; n < i; n++)
-	{
-		if (s[n] == c)
-		{
-			j = 0;
-			for (n; n < i; n++)
-			{
-				s[j] = y[n];
-				j++;
-			}
-			break;
-		}
-		else if (n == i - 1)
-			s = '\0';
-		else
-			continue;
-	}
-	return (s);
+	for (*s; *s != '\0'; s++)
+		if (*s == c)
+			return (s);
+	return ('\0');
 }
