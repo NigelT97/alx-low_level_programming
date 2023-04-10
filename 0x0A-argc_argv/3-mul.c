41da56fa6ae1435ba72297eaf01a434763719dae
NigelT97 <NigelT97>
2023-04-10 11:11:30 +0000

    question 3 fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
 	int i, x = 1;
 
 	if (argc != 3)
+	{
+		printf("Error\n");
 		return (1);
-
+	}
 	for (i = 1; i < 3; i++)
 		x *= atoi(argv[i]);
 	printf("%d\n", x);
