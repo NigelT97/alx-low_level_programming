dbf1d5c10b7db8d5857d392a3dde1eba66ba9842
NigelT97 <NigelT97>
2023-04-10 11:32:38 +0000

    question 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - add numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 1 if error and 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, x = 0, flag;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]))
+		{
+			x += atoi(argv[i]);
+			flag = 0;
+		}
+		else
+			flag = 1;
+	}
+	if (flag == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", x);
+	return (0);
+}
