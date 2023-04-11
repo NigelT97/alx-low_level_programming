33b25d17c3f51060107451cbbf93036df304eaa2
NigelT97 <NigelT97>
2023-04-11 11:49:46 +0000

    advance question 1

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - calculate the minimum coins required from change
+ * @argc: change amount
+ * @argv: array of change
+ * Return: 1 when error and zero when succesful
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, x, y = 0;
+	int change = {25, 10, 5, 2, 1}
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	for (i = 0, i < 5, i++)
+	{
+		for (j = 0; x >= change[i]; j++)
+		{
+			x -= change[i];
+			y++;
+		}
+	}
+	printf("%d\n", y);
+	return (0);
+}			
