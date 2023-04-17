c1d1f5aad73a135f620db2f505f470dc42d16024
NigelT97 <NigelT97>
2023-04-17 20:04:00 +0000

    question 0 fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct dog for info
+ * @name: - pointer to name
+ * @age: - float
+ * @owner: - string
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+#endif
