05fdec6b0cf3c58b3a376349b318267159523164
NigelT97 <NigelT97>
2023-04-19 21:26:47 +0000

    question 3 fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,16 @@
 #include "3-calc.h"
 /**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+/**
  * get_op_func - pointer to function
  * @s: string to operator
  * Return: operator type
