d6e38d97bdc9d12b5cab24bd074effcf121126a4
NigelT97 <NigelT97>
2023-04-19 22:33:29 +0000

    extra work 1

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entrance into programmme
+ * Description: prints the opcodes of its own main funtcion
+ * @argc: number of entries
+ * @argv: array pointer
+ * Return: success zero
+ */
+int main(int argc, char *argv[])
+{
+	int i, b;
+	char *a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = (char *)main;
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", a[i]);
+			break;
+		}
+		printf("%02hhx ", a[i]);
+	}
+	return (0);
+}
