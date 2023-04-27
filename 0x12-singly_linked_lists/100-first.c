1dbde37ada676b1308f9edfe2a7505a4733a5287
NigelT97 <NigelT97>
2023-04-27 22:02:14 +0000

    question 100 minor fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,5 @@
 void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n I bore");
-	printf(" my house upon my back!\n);
+	printf(" my house upon my back!\n");
 }
