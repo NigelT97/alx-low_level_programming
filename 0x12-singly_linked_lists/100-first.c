f51865605a945fd753452325680adf69f4b128e0
NigelT97 <NigelT97>
2023-04-27 22:00:08 +0000

    question 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * premain - before main function
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n I bore");
+	printf(" my house upon my back!\n);
+}
