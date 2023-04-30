6286dc0076bca8dc65c7037d9626252284f2351c
NigelT97 <NigelT97>
2023-04-30 19:23:10 +0000

    question 9 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,26 +16,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (nn == NULL || (idx != 0 && *head == NULL))
 		return (NULL);
 	nn->n = n;
-	for (i = 0; i < idx - 1 && head != NULL; i++)
+	nn->next = NULL;
+	for (i = 0; i < idx && *head != NULL; i++)
 	{
-		c = c->next;
-		if (c == NULL)
-			return (NULL);
+		if (i == idx - 1)
+		{
+			nn->next = c->next;
+			c->next = nn;
+			return (nn);
+		}
+		else
+			c = c->next;
 	}
 	if (idx == 0)
 	{
 		nn->next = *head;
 		*head = nn;
+		return (nn);
 	}
-	else if (c->next)
-	{
-		nn->next = c->next;
-		c->next = nn;
-	}
-	else
-	{
-		nn->next = NULL;
-		c->next = nn;
-	}
-	return (nn);
+	return (NULL);
 }
