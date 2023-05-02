48b56910386af5e9d182884c67623ae174a09607
NigelT97 <NigelT97>
2023-05-02 19:35:13 +0000

    question 13 extra

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frunction that frees list
+ * @h: pointer to first node
+ * Return: size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t ln = 0;
+	int d;
+	listint_t *c;
+
+	if (!h || !*h)
+		return (0);
+
+	for (; *h;)
+	{
+		d = *h - (*h)->next;
+		if (d > 0)
+		{
+			c = (*h)->next;
+			free(*h);
+			*h = c;
+			ln++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			ln++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (ln);
+}
