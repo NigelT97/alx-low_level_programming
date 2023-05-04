4323435ffa3802e814fcef96f96ade2acb5dafe3
NigelT97 <NigelT97>
2023-05-04 19:33:55 +0000

    question 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - prints binary
+ * @n: number to be printed
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int ni;
+	int i;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	for (ni = n, i = 0; (ni >>= 1) > 0; i++)
+		continue;
+	while (i >= 0)
+	{
+		if ((n >> i) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
